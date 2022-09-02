8b6214c5a405bb1dd6cf96f4f6a78426f9860aa1
Lorenz Hambsch <lorenz.hambsch@vito.be>
2022-09-02 16:26:19 +0200

    fix schema

--- server/guppy2/db/models.py
+++ server/guppy2/db/models.py
@@ -1,16 +1,16 @@
 # coding: utf-8
 from sqlalchemy import Column, Integer, String, text, Boolean, Float
 from sqlalchemy.ext.declarative import declarative_base
-
+from ..config import config
 Base = declarative_base()
 metadata = Base.metadata
 
 
 class LayerMetadata(Base):
     __tablename__ = 'layer_metadata'
-    __table_args__ = {'schema': 'guppy2'}
+    __table_args__ = {'schema': config.database.db}
 
-    id = Column(Integer, primary_key=True, server_default=text("nextval('guppy2.layer_metadata_seq'::regclass)"))
+    id = Column(Integer, primary_key=True, server_default=text(f"nextval('{config.database.db}.layer_metadata_seq'::regclass)"))
     layer_name = Column(String, nullable=False, unique=True)
     file_path = Column(String, nullable=False)
     is_rgb = Column(Boolean)
