c8dde297df4a944a5d768056288d5a7cf4d448c8
Lorenz Hambsch <lorenz.hambsch@vito.be>
2023-08-11 09:35:27 +0200

    reduce memory needed

--- server/guppy2/endpoints_calc.py
+++ server/guppy2/endpoints_calc.py
@@ -38,7 +38,7 @@ def raster_calculation(db: Session, body: s.RasterCalculationBody):
             arguments_list.append({'nodata': nodata, 'factor': layer_item.factor, 'operation': layer_item.operation, 'is_rgb': layer_model.is_rgb})
     process_raster_list_with_function_in_chunks(path_list, os.path.join(base_path, raster_name), path_list[0],
                                                 function_to_apply=perform_operation, function_arguments={'layer_args': arguments_list, 'output_rgb': body.rgb},
-                                                chunks=10, output_bands=4 if body.rgb else 1, dtype=np.uint8 if body.rgb else None, out_nodata=255 if body.rgb else None)
+                                                chunks=20, output_bands=4 if body.rgb else 1, dtype=np.uint8 if body.rgb else None, out_nodata=255 if body.rgb else None)
     build_overview_tiles = [2, 4, 8, 16, 32, 64]
     image = gdal.Open(os.path.join(base_path, raster_name), 1)  # 0 = read-only, 1 = read-write.
     gdal.SetConfigOption('COMPRESS_OVERVIEW', 'DEFLATE')
