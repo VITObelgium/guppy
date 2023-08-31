fix calculation, add result style to generation

server/guppy2/endpoints_calc.py
@@ -48,7 +48,11 @@ def raster_calculation(db: Session, body: s.RasterCalculationBody):
             fixed_path_list.append(file.replace(".tif", f"_{unique_identifier}.tif"))
         else:
             fixed_path_list.append(file)
-
+    unique_values = []
+    for path in fixed_path_list:
+        with rasterio.open(path) as ds:
+            input_arr = ds.read(out_shape=(int(ds.height / 4), int(ds.width / 4)))
+        unique_values.append(np.unique(input_arr))
     process_raster_list_with_function_in_chunks(fixed_path_list, os.path.join(base_path, raster_name), path_list[0],
                                                 function_to_apply=perform_operation, function_arguments={'layer_args': arguments_list, 'output_rgb': body.rgb},
                                                 chunks=20, output_bands=4 if body.rgb else 1, dtype=np.uint8 if body.rgb else None, out_nodata=255 if body.rgb else None)
@@ -92,7 +96,7 @@ def raster_calculation(db: Session, body: s.RasterCalculationBody):
     print('raster_calculation 200', time.time() - t)
     insert_into_guppy_db(db, raster_name, os.path.join(base_path, raster_name), body.rgb)
     if body.geoserver:
-        geoserver_layer = create_raster(raster_name)
+        geoserver_layer = create_raster(raster_name, body.result_style)
         return Response(content=geoserver_layer, status_code=status.HTTP_201_CREATED)
     else:
         return generate_raster_response(os.path.join(base_path, raster_name))

server/guppy2/raster_calc_utils.py
@@ -19,10 +19,10 @@ from guppy2.error import create_error
 from guppy2.rasterio_file_streamer import RIOFile
 
 
-def create_raster(raster_name):
+def create_raster(raster_name, style=None):
     t = time.time()
     try:
-        geoserver_layer = create_geoserver_layer(raster_name, raster_name.split('.')[0])
+        geoserver_layer = create_geoserver_layer(raster_name, raster_name.split('.')[0], style)
         print("done geoserver", time.time() - t)
         return geoserver_layer
     except requests.exceptions.ConnectionError as e:
@@ -36,7 +36,7 @@ def insert_into_guppy_db(db: Session, filename, file_path, is_rgb):
     db.commit()
 
 
-def create_geoserver_layer(data_source, layer_name):
+def create_geoserver_layer(data_source, layer_name, sld_name=None):
     # Set up authentication (if required)
     username = cfg.geoserver.username
     password = cfg.geoserver.password
@@ -87,6 +87,13 @@ def create_geoserver_layer(data_source, layer_name):
     else:
         print(f"Failed to create layer. Status code: {response.status_code}")
         print(response.text)
+    if sld_name and len(str(sld_name).strip()) > 0 and str(sld_name).strip() != 'nan':
+        url = base_url + f'workspaces/{workspace}/layers/{layer_name}.xml'
+        headers = {'content-type': 'text/xml', 'Accept-Charset': 'UTF-8'}
+        data = f'<layer><defaultStyle><name>{sld_name}</name></defaultStyle></layer>'
+        r = requests.put(url, data=data, headers=headers, auth=auth)
+        if not r.ok:
+            print(layer_name, 'set style failed', r)
     return f"{workspace}:{layer_name}"
 
 
@@ -129,7 +136,7 @@ def perform_operation(*input_arrs, layer_args, output_rgb):
                 combo = itertools.product(np.unique(output_arr[~np.isnan(output_arr)]), np.unique(input_arr[~np.isnan(input_arr)]))
                 combo_arr = output_arr.copy()
                 for idx, combination in enumerate(combo):
-                    combo_arr[(np.where(output_arr == combination[0]) & (input_arr == combination[1]))] = idx
+                    combo_arr[np.where((output_arr == combination[0]) & (input_arr == combination[1]))] = idx
                 output_arr = combo_arr.copy()
     if output_rgb:
         output_arr = data_to_rgba(output_arr, out_nodata)