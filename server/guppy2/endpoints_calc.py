6a90598a743a3b7a9aaa157856c8001d63054a97
Lorenz Hambsch <lorenz.hambsch@vito.be>
2023-09-08 13:18:33 +0200

    skip pyramid creation for pre temp raster because its slow.

--- server/guppy2/endpoints_calc.py
+++ server/guppy2/endpoints_calc.py
@@ -54,32 +54,29 @@ def raster_calculation(db: Session, body: s.RasterCalculationBody):
             input_arr = ds.read(out_shape=(int(ds.height / 4), int(ds.width / 4)))
         unique_values.append(np.unique(input_arr))
     process_raster_list_with_function_in_chunks(fixed_path_list, os.path.join(base_path, raster_name), path_list[0],
-                                                function_to_apply=perform_operation, function_arguments={'layer_args': arguments_list, 'output_rgb': body.rgb},
-                                                chunks=20, output_bands=4 if body.rgb else 1, dtype=np.uint8 if body.rgb else None, out_nodata=255 if body.rgb else None)
-    build_overview_tiles = [2, 4, 8, 16, 32, 64]
-    image = gdal.Open(os.path.join(base_path, raster_name), 1)  # 0 = read-only, 1 = read-write.
-    gdal.SetConfigOption('COMPRESS_OVERVIEW', 'DEFLATE')
-    image.BuildOverviews("NEAREST", build_overview_tiles)
-    del image
+                                                function_to_apply=perform_operation, function_arguments={'layer_args': arguments_list, 'output_rgb': body.rgb, 'unique_values': unique_values},
+                                                chunks=10, output_bands=4 if body.rgb else 1, dtype=np.uint8 if body.rgb else None, out_nodata=255 if body.rgb else None)
+    print('2', time.time() - t)
     if body.rescale_result:
         os.rename(src=os.path.join(base_path, raster_name), dst=os.path.join(base_path, raster_name.replace('.tif', 'tmp.tif')))
-        with rasterio.open(os.path.join(base_path, raster_name.replace('.tif', 'tmp.tif'))) as ds:
-            input_arr = ds.read(out_shape=(int(ds.height / 4), int(ds.width / 4)))
-        input_arr = np.where(input_arr == nodata, np.nan, input_arr)
-        input_arr = input_arr[~np.isnan(input_arr)]
-        if body.rescale_result.rescale_type == s.AllowedRescaleTypes.quantile:
-            rescale_result_list = [np.nanquantile(input_arr, b) for b in body.rescale_result.breaks]
-            rescale_result_dict = {k: v for k, v in enumerate(rescale_result_list)}
-        elif body.rescale_result.rescale_type == s.AllowedRescaleTypes.equal_interval:
-            input_arr *= 1.0 / input_arr.max()
-            rescale_result_list = body.rescale_result.breaks
-            rescale_result_dict = {k: v for k, v in enumerate(rescale_result_list)}
-        elif body.rescale_result.rescale_type == s.AllowedRescaleTypes.natural_breaks:
-            input_arr *= 1.0 / input_arr.max()
-            sample_arr = np.random.choice(input_arr[input_arr != 0], size=10000)  # needs low samples or jenks is too slow
-            rescale_result_list = jenkspy.jenks_breaks(sample_arr, n_classes=len(body.rescale_result.breaks))
-            rescale_result_dict = {k: v for k, v in enumerate(rescale_result_list)}
-        elif body.rescale_result.rescale_type == s.AllowedRescaleTypes.provided:
+        if body.rescale_result.rescale_type != s.AllowedRescaleTypes.provided:
+            with rasterio.open(os.path.join(base_path, raster_name.replace('.tif', 'tmp.tif'))) as ds:
+                input_arr = ds.read(1)
+            input_arr = np.where(input_arr == nodata, np.nan, input_arr)
+            input_arr = input_arr[~np.isnan(input_arr)]
+            if body.rescale_result.rescale_type == s.AllowedRescaleTypes.quantile:
+                rescale_result_list = [np.nanquantile(input_arr, b) for b in body.rescale_result.breaks]
+                rescale_result_dict = {k: v for k, v in enumerate(rescale_result_list)}
+            elif body.rescale_result.rescale_type == s.AllowedRescaleTypes.equal_interval:
+                input_arr *= 1.0 / input_arr.max()
+                rescale_result_list = body.rescale_result.breaks
+                rescale_result_dict = {k: v for k, v in enumerate(rescale_result_list)}
+            elif body.rescale_result.rescale_type == s.AllowedRescaleTypes.natural_breaks:
+                input_arr *= 1.0 / input_arr.max()
+                sample_arr = np.random.choice(input_arr[input_arr != 0], size=10000)  # needs low samples or jenks is too slow
+                rescale_result_list = jenkspy.jenks_breaks(sample_arr, n_classes=len(body.rescale_result.breaks))
+                rescale_result_dict = {k: v for k, v in enumerate(rescale_result_list)}
+        else:
             rescale_result_dict = body.rescale_result.breaks
         print(rescale_result_dict)
         process_raster_list_with_function_in_chunks([os.path.join(base_path, raster_name.replace('.tif', 'tmp.tif'))], os.path.join(base_path, raster_name),
