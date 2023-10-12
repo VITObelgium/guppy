e8d9433a93d20ecc2fe6a35330bddd334d8cb410
Lorenz Hambsch <lorenz.hambsch@vito.be>
2023-10-12 15:52:04 +0200

    make binned output int maps

--- server/guppy2/endpoints_calc.py
+++ server/guppy2/endpoints_calc.py
@@ -93,8 +93,8 @@ def raster_calculation(db: Session, body: s.RasterCalculationBody):
         process_raster_list_with_function_in_chunks([os.path.join(base_path, raster_name.replace('.tif', 'tmp.tif'))], os.path.join(base_path, raster_name),
                                                     os.path.join(base_path, raster_name.replace('.tif', 'tmp.tif')),
                                                     function_to_apply=rescale_result,
-                                                    function_arguments={'output_rgb': body.rgb, 'rescale_result_dict': rescale_result_dict, 'nodata': arguments_list[0]['nodata'], 'bins':bins},
-                                                    chunks=10, output_bands=4 if body.rgb else 1, dtype=np.uint8 if body.rgb else None, out_nodata=255 if body.rgb else None)
+                                                    function_arguments={'output_rgb': body.rgb, 'rescale_result_dict': rescale_result_dict, 'nodata': arguments_list[0]['nodata'], 'bins': bins},
+                                                    chunks=10, output_bands=4 if body.rgb else 1, dtype=np.uint8 if (body.rgb or bins) else None, out_nodata=255 if body.rgb else None)
     build_overview_tiles = [2, 4, 8, 16, 32, 64]
     image = gdal.Open(os.path.join(base_path, raster_name), 1)  # 0 = read-only, 1 = read-write.
     gdal.SetConfigOption('COMPRESS_OVERVIEW', 'DEFLATE')
