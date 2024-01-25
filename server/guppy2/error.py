77eb67c3ed6947d990c6ccb7453079fd4eef0182
Lorenz Hambsch <lorenz.hambsch@vito.be>
2024-01-25 14:02:49 +0100

    type and document function

--- server/guppy2/error.py
+++ server/guppy2/error.py
@@ -1,5 +1,14 @@
 from fastapi import HTTPException
 
 
-def create_error(message, code=400):
+def create_error(message: str, code: int = 400) -> HTTPException:
+    """
+    Args:
+        message: A string representing the error message.
+        code: An integer representing the HTTP status code for the error. Default is 400.
+
+    Raises:
+        HTTPException: An exception indicating an HTTP error with the specified error message and status code.
+
+    """
     raise HTTPException(status_code=code, detail=message)
