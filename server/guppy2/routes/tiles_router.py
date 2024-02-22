1d531d5b1882469e9fe257375f2e0214aad45f06
Lorenz Hambsch <lorenz.hambsch@vito.be>
2024-02-22 16:13:25 +0100

    add list of possible styles to description

--- server/guppy2/routes/tiles_router.py
+++ server/guppy2/routes/tiles_router.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
+from rio_tiler.colormap import cmap
 from sqlalchemy.orm import Session
 from starlette.responses import Response
 
@@ -19,5 +20,7 @@ async def get_vector_tile(layer_name: str, z: int, x: int, y: int, db: Session =
 
 @router.get(r"/raster/{layer_name}/{z}/{x}/{y}.png", responses={200: {"content": {"image/png": {}}, "description": "Return an image.", }}, response_class=Response,
             description="Read COG and return a png tile")
-async def get_raster_tile(layer_name: str, z: int, x: int, y: int, style: str = None, db: Session = Depends(get_db)):
+async def get_raster_tile(layer_name: str, z: int, x: int, y: int,
+                          style: str = Query(..., description=f"Style should be 'shader_rgba' or one of {list(cmap.data.keys())} values"),
+                          db: Session = Depends(get_db)):
     return endpoints_rio_tiler.get_tile_for_layer(layer_name=layer_name, db=db, z=z, x=x, y=y, style=style)
