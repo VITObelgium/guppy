77d6b42755f3243e72d4eeeb4329c3e93520f252
Lorenz Hambsch <lorenz.hambsch@vito.be>
2024-02-29 16:58:37 +0100

    update dbinit

--- server/guppy2/db/db_init.sql
+++ server/guppy2/db/db_init.sql
@@ -6,6 +6,14 @@ CREATE TABLE "layer_metadata" (
   "rgb_factor" float,
   "is_mbtile" boolean NOT NULL
 );
+ CREATE TABLE "tile_statistics" (
+   id SERIAL PRIMARY KEY,
+   layer_name VARCHAR NOT NULL,
+   x INTEGER NOT NULL,
+   y INTEGER NOT NULL,
+   z INTEGER NOT NULL,
+   count INTEGER NOT NULL
+ );
 
 GRANT ALL ON all tables IN SCHEMA guppy2 TO guppy2;
 GRANT ALL ON all sequences IN SCHEMA guppy2 TO guppy2;
