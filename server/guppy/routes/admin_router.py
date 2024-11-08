6336d890314adddd8d0de6e9072c4890e4c5fe97
Lorenz Hambsch <lorenz.hambsch@vito.be>
2024-11-08 11:50:57 +0100

    fix issue with missing data in upload

--- server/guppy/routes/admin_router.py
+++ server/guppy/routes/admin_router.py
@@ -14,7 +14,7 @@ router = APIRouter(
 
 
 @router.post("/upload", description="Upload a file (GeoTiff or Gpkg) to the server.")
-def upload_file(layerName: str = Form(...), layerLabel: str = Form(...), isRgb: bool = Form(False), maxZoom: int = Form(17), file: UploadFile = File(...), data: UploadFile = File(defaut=None),
+def upload_file(layerName: str = Form(...), layerLabel: str = Form(...), isRgb: bool = Form(False), maxZoom: int = Form(17), file: UploadFile = File(...), data: UploadFile = File(None),
                 db: Session = Depends(get_db)):
     return endpoints_upload.upload_file(layer_name=layerName, label=layerLabel, file=file, data=data, is_rgb=isRgb, max_zoom=maxZoom, db=db)
 
