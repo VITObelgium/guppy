force int32 of float32 dtypes. fix overflow issues on classification on non float32 dypes.

server/guppy/endpoints/endpoints.py
@@ -435,11 +435,14 @@ def get_classification_for_wkt(db: Session, layer_name: str, body: s.GeometryBod
                                 shape_mask = _extract_shape_mask_from_dataset(src, [geom],all_touched=True, crop=True)
                             response = _calculate_classification_polygon_method(rst, shape_mask, geom, src, crop_transfrom)
                         else:
-                            values, counts = np.unique(np.where(shape_mask == 0, rst, -999999999999), return_counts=True)
+                            mask_value = -999999999999
+                            if rst.dtype == np.int32:
+                                mask_value = -2147483647
+                            values, counts = np.unique(np.where(shape_mask == 0, rst, mask_value), return_counts=True)
                             result_classes = []
-                            total_count = sum([c for v, c in zip(values, counts) if v != -999999999999])
+                            total_count = sum([c for v, c in zip(values, counts) if v != mask_value])
                             for v, c in zip(values, counts):
-                                if v != -999999999999:
+                                if v != mask_value:
                                     result_classes.append(s.ClassificationEntry(value=v, count=c, percentage=c / total_count * 100))
                             response = s.ClassificationResult(type='classification', data=result_classes)
                         logger.info(f'classification_for_wkt 200 {time.time() - t}')

server/guppy/endpoints/upload_utils.py
@@ -285,10 +285,15 @@ def save_geotif_tiled_overviews(input_file: str, output_file: str, nodata: int) 
     with rasterio.open(input_file) as src:
         target_crs = rasterio.crs.CRS.from_epsg(code=3857)
         tmp_input_file = None
-        if src.crs != target_crs or nodata != src.nodata:
+        if np.issubdtype(src.dtypes[0], np.floating):
+            target_dtype = np.float32
+        else:
+            target_dtype = np.int32
+
+        if src.crs != target_crs or nodata != src.nodata or src.dtypes[0] != target_dtype:
             transform, width, height = rasterio.warp.calculate_default_transform(src.crs, target_crs, src.width, src.height, *src.bounds)
             profile = src.profile
-            profile.update(crs=target_crs, transform=transform, width=width, height=height)
+            profile.update(crs=target_crs, transform=transform, width=width, height=height, dtype=target_dtype)
             tmp_input_file = input_file.replace('.tif', '_tmp.tif')
             with rasterio.open(tmp_input_file, 'w', **profile) as dst:
                 for i in range(1, src.count + 1):